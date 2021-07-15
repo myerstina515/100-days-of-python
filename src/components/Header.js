f5798ff50b497ed5e180f5450d2821083b853960
TINA MYERS <myers.tina515@gmail.com>
2021-07-14 17:19:46 -0700

    layout, CSS, and adding day 1 & 2

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+          <header>
+            Tina Myers 100 Days of Python!
+          </header>
+    </div>
+  )
+}
+export default Header;
