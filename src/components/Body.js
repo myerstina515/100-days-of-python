e3d15f531219364353e7705a8cd83f6464ee7cd4
TINA MYERS <myers.tina515@gmail.com>
2021-07-19 15:07:03 -0700

    adding day 3

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,8 +6,18 @@ import Button from '@material-ui/core/Button';
 import "./Body.scss";
 import Day1 from '../assets/PythonDay1.jpg'
 import Day2 from '../assets/PythonDay2.jpg'
+import Day3 from '../assets/PythonDay3.jpg'
 
 const Body = () => {
+  const images = [Day1, Day2]
+  const title = ["Day 1: Band Name Generator", 
+    "Day 2: Tip Calculator",
+  ]
+  const link = [
+    "https://replit.com/@TinaFielder/band-name-generator-start#main.py", 
+    "https://replit.com/@TinaFielder/tip-calculator-start#main.py"
+  ]
+
   return (
     <div className="container">
         <Card className="root" variant="outlined">
@@ -16,7 +26,7 @@ const Body = () => {
             <Typography className="question" color="textSecondary" gutterBottom>
               Day 1: Band Name Generator
             </Typography>
-            <Button href="https://replit.com/@TinaFielder/band-name-generator-start#main.py">Go see it!</Button>
+            <Button href="https://replit.com/@TinaFielder/band-name-generator-start?v=1">Go see it!</Button>
           </CardContent>
         </Card>
         <Card className="root" variant="outlined">
@@ -25,7 +35,16 @@ const Body = () => {
             <Typography className="question" color="textSecondary" gutterBottom>
               Day 2: Tip Calculator
             </Typography>
-            <Button href="https://replit.com/@TinaFielder/tip-calculator-start#main.py">Go see it!</Button>
+            <Button href="https://replit.com/@TinaFielder/tip-calculator-start?v=1">Go see it!</Button>
+          </CardContent>
+        </Card>
+        <Card className="root" variant="outlined">
+          <CardContent class="backContent">
+            <img id="day1" alt="card content" src={Day3} />
+            <Typography className="question" color="textSecondary" gutterBottom>
+              Day 23: Choose your Adventure!
+            </Typography>
+            <Button href="https://replit.com/@TinaFielder/treasure-island-start?v=1">Go see it!</Button>
           </CardContent>
         </Card>
     </div>
