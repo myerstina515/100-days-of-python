4447de48434a788d0c64d30cd91c3c9da0e711ee
TINA MYERS <myers.tina515@gmail.com>
2021-07-28 20:12:53 -0700

    optimized code and added day 4-7

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,59 +4,25 @@ import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import "./Body.scss";
-import Day1 from '../assets/PythonDay1.jpg';
-import Day2 from '../assets/PythonDay2.jpg';
-import Day3 from '../assets/PythonDay3.jpg';
-import Day4 from '../assets/PythonDay4.jpg';
+import {days} from './days.js';
 
 const Body = () => {
-  const images = [Day1, Day2]
-  const title = ["Day 1: Band Name Generator", 
-    "Day 2: Tip Calculator",
-  ]
-  const link = [
-    "https://replit.com/@TinaFielder/band-name-generator-start#main.py", 
-    "https://replit.com/@TinaFielder/tip-calculator-start#main.py"
-  ]
+
+  
 
   return (
     <div className="container">
-        <Card className="root" variant="outlined">
-          <CardContent class="backContent">
-            <img id="day1" alt="card content" src={Day1} />
+      {days.map((x) => (
+        <Card key={x.id} className="root" variant="outlined">
+          <CardContent className="backContent">
+            <img id="day1" alt="card content" src={x.images} />
             <Typography className="question" color="textSecondary" gutterBottom>
-              Day 1: Band Name Generator
+              {x.title}
             </Typography>
-            <Button href="https://replit.com/@TinaFielder/band-name-generator-start?v=1">Go see it!</Button>
+            <Button href={x.link}>Go see it!</Button>
           </CardContent>
         </Card>
-        <Card className="root" variant="outlined">
-          <CardContent class="backContent">
-            <img id="day1" alt="card content" src={Day2} />
-            <Typography className="question" color="textSecondary" gutterBottom>
-              Day 2: Tip Calculator
-            </Typography>
-            <Button href="https://replit.com/@TinaFielder/tip-calculator-start?v=1">Go see it!</Button>
-          </CardContent>
-        </Card>
-        <Card className="root" variant="outlined">
-          <CardContent class="backContent">
-            <img id="day1" alt="card content" src={Day3} />
-            <Typography className="question" color="textSecondary" gutterBottom>
-              Day 23: Choose your Adventure!
-            </Typography>
-            <Button href="https://replit.com/@TinaFielder/treasure-island-start?v=1">Go see it!</Button>
-          </CardContent>
-        </Card>
-        <Card className="root" variant="outlined">
-          <CardContent class="backContent">
-            <img id="day1" alt="card content" src={Day4} />
-            <Typography className="question" color="textSecondary" gutterBottom>
-              Rock, Paper, Scissors
-            </Typography>
-            <Button href="https://replit.com/@TinaFielder/rock-paper-scissors-start?v=1">Go see it!</Button>
-          </CardContent>
-        </Card>
+      ))}
     </div>
   )
 }
